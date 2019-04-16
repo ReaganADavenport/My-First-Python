45dfceb7ab2aecdb95d6bedcb07ba6595170337e
Reagan Davenport <reagandavenport@reagans-air.attlocal.net>
2019-04-16 17:26:53 -0400

    First Commit

---
+++ Python-AddLibs.py
@@ -0,0 +1,8 @@
+NAME = input("Please provide a name: ")
+ANIMAL = input("Please provide an animal: ")
+ITEM = input("Please provide an inanimate item: ")
+GROUP = input("Please provide a plural noun (ex: dogs): ")
+NOUN = input("Please provide a singular noun: ")
+
+
+print("%s defeated the %s with the %s and made the %s respect %s." % (NAME, ANIMAL, ITEM, GROUP, NOUN))
---
+++ Python-Project.py
@@ -0,0 +1,5 @@
+FIRSTNAME = input("What is your first name? ")
+LASTNAME = input("What is your last name? ")
+COLOR = input("What is your favorite color? ")
+
+print("Hello, %s %s! %s is my favorite color too!" % (FIRSTNAME, LASTNAME, COLOR))
